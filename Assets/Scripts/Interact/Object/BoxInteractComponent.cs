d81b1d5e7541ef5214b0ba81ee1913c17b088f37
Kimikhwan <moonfgg@naver.com>
2025-11-25 13:51:08 +0900

    Feat[FC_1]: 상자 상호작용

--- Assets/Scripts/Interact/Object/BoxInteractComponent.cs
+++ Assets/Scripts/Interact/Object/BoxInteractComponent.cs
@@ -1,10 +1,32 @@
+using GameUI;
 using UnityEngine;
 
 namespace GameInteract
 {
     public class BoxInteractComponent : InteractBaseComponent
     {
+        Animation anim;
 
+        void Start()
+        {
+            anim = GetComponent<Animation>();
+        }
+
+        public override void Interact(IEntity entity)
+        {
+            base.Interact(entity);
+
+            OnAnimation();
+        }
+
+        void OnAnimation()
+        {
+            anim.Play();
+            EndInteract();
+
+            var popUp = Manager.UI.ShowPopup<GetItemPopUp>();
+            popUp.SetData("10080072", 10);
+        }
     }
 }
 
